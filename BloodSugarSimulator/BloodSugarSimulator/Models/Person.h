7079792f524f4e96ca811ec7fbfa578a85088d70
KK TOCC <kaushal@theorangechef.com>
2015-08-09 16:14:16 -0700

    Add function descriptors for Person class.

--- BloodSugarSimulator/BloodSugarSimulator/Models/Person.h
+++ BloodSugarSimulator/BloodSugarSimulator/Models/Person.h
@@ -7,13 +7,22 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Food.h"
+#import "Exercise.h"
 
 @interface Person : NSObject
 
 @property (nonatomic, readwrite) NSString* name;
+@property (nonatomic, readonly) NSDate* initializationTime;
 @property (nonatomic, assign) BOOL influenceOfFood;
 @property (nonatomic, assign) BOOL influenceOfExercise;
 @property (nonatomic, readwrite) NSTimer* foodTimer;
 @property (nonatomic, readwrite) NSTimer* exerciseTimer;
+@property (nonatomic, readwrite) NSMutableArray* bloodSugarModifiers;
+
+- (float) getCurrentBloodSugar:(NSDate *)time;
+- (float) getGlycation:(NSDate *)time;
+- (void) logFood:(Food *)foodItem atTime:(NSDate *)time;
+- (void) logExercise:(Exercise *)exerciseType atTime:(NSDate *)time;
 
 @end
--- BloodSugarSimulator/BloodSugarSimulator/Models/Person.m
+++ BloodSugarSimulator/BloodSugarSimulator/Models/Person.m
@@ -10,4 +10,33 @@
 
 @implementation Person
 
+
+
+- (float) getCurrentBloodSugar:(NSDate *)time
+{}
+
+- (float) getGlycation:(NSDate *)time
+{}
+
+- (void) logFood:(Food *)foodItem atTime:(NSDate *)time
+{
+    
+}
+
+- (void) logExercise:(Exercise *)exerciseType atTime:(NSDate *)time
+{}
+
+
+#pragma mark internal routines
+
+- (float) calculateBloodSugar:(NSDate *)time
+{
+
+}
+
+- (float) calculateGlycation:(NSDAte *)time
+{
+
+}
+
 @end
