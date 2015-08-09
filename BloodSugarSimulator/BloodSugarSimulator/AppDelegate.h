6934ff1b07f6f65da6e8af054d591214d86657c2
KK TOCC <kaushal@theorangechef.com>
2015-08-09 14:06:43 -0700

    Create XCode project

---
+++ BloodSugarSimulator/BloodSugarSimulator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BloodSugarSimulator
+//
+//  Created by Kaushal Kantawala on 8/9/15.
+//  Copyright (c) 2015 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BloodSugarSimulator/BloodSugarSimulator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BloodSugarSimulator
+//
+//  Created by Kaushal Kantawala on 8/9/15.
+//  Copyright (c) 2015 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
